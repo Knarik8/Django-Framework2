1ccb63270b581b64a28f4c557b47273549a96ff0
Knarik8 <k_armina@mail.ru>
2021-07-14 15:55:43 +0300

    lesson7

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -168,9 +168,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "geekshop", "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "geekshop", "static"),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 #для поиска папок со статикой внутри папок приложения

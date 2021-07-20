55439ab96ef1dc319dd117e5e4a5748a6077e49d
Knarik8 <k_armina@mail.ru>
2021-07-20 12:33:44 +0300

    cancel last commit

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -43,8 +43,7 @@ def products(request, pk=None, page=1):
             }
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            # products = Product.objects.filter(category_id__pk=pk).order_by('price')
-            products = Product.objects.filter(category_id__pk=pk).select_related('category')[:3]
+            products = Product.objects.filter(category_id__pk=pk).order_by('price')
 
 
         paginator=Paginator(products, 2)

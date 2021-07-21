8b492d274906574879e14b4e703555dcb95eefad
Knarik8 <k_armina@mail.ru>
2021-07-21 22:02:07 +0300

    add select_related in geekshop_views     products = Product.objects.filter().select_related('category')[:3]

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -6,7 +6,9 @@ from basketapp.models import Basket
 
 def main(request):
 
-    products = Product.objects.all()[:3]
+    # products = Product.objects.all()[:3]
+    products = Product.objects.filter().select_related('category')[:3]
+
 
     basket=''
     if request.user.is_authenticated:

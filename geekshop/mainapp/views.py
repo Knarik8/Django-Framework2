6095c1c49f3661d7905fe073f9ef4e589ed1a8b6
Knarik8 <k_armina@mail.ru>
2021-07-23 22:29:03 +0300

    change def products

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -50,52 +50,106 @@ def get_same_products(hot_products):
     same_products = Product.objects.filter(category=hot_products.category).exclude(pk=hot_products.pk)[:3]
 
     return same_products
+#
+# def products(request, pk=None, page=1):
+#     title = 'продукты'
+#     category = ''
+#     products = ''
+#     # links_menu = ProductCategory.objects.all()
+#
+#     categories = ProductCategory.objects.all()
+#     # basket = get_basket(request.user)
+#
+#     if pk:
+#         if pk == 0:
+#             category = {
+#                 'pk': 0,
+#                 'name': 'Все'
+#             }
+#         else:
+#             category = get_object_or_404(ProductCategory, pk=pk)
+#             # products = Product.objects.filter(category_id__pk=pk).order_by('price')
+#             products = Product.objects.filter(category_id__pk=pk).select_related().order_by('price')
+#
+#
+#         paginator=Paginator(products, 2)
+#         try:
+#             products_paginator=paginator.page(page)
+#         except PageNotAnInteger:
+#             products_paginator=paginator.page(1)
+#         except EmptyPage:
+#             products_paginator=paginator.page(paginator.num_pages)
+#
+#
+#     hot_product = get_hot_product()
+#     same_products = get_same_products(hot_product)
+#
+#     context = {
+#         'title': title,
+#         'categories': categories,
+#         'category': category,
+#         'links_menu': get_links_menu(),
+#         'products': products_paginator,
+#         # 'basket': basket,
+#         'hot_product': hot_product,
+#         'same_products': same_products,
+#     }
+#     return render(request, 'products_list.html', context=context)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
 
 def products(request, pk=None, page=1):
     title = 'продукты'
-    category = ''
-    products = ''
-    # links_menu = ProductCategory.objects.all()
-
-    categories = ProductCategory.objects.all()
-    # basket = get_basket(request.user)
+    links_menu = ProductCategory.objects.filter(is_active=True)
+    basket = get_basket(request.user)
 
     if pk:
-        if pk == 0:
+        if pk == '0':
             category = {
                 'pk': 0,
-                'name': 'Все'
+                'name': 'все'
             }
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            # products = Product.objects.filter(category_id__pk=pk).order_by('price')
-            products = Product.objects.filter(category_id__pk=pk).select_related().order_by('price')
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
+                'price')
 
-
-        paginator=Paginator(products, 2)
+        paginator = Paginator(products, 2)
         try:
-            products_paginator=paginator.page(page)
+            products_paginator = paginator.page(page)
         except PageNotAnInteger:
-            products_paginator=paginator.page(1)
+            products_paginator = paginator.page(1)
         except EmptyPage:
-            products_paginator=paginator.page(paginator.num_pages)
+            products_paginator = paginator.page(paginator.num_pages)
 
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products_paginator,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
 
     hot_product = get_hot_product()
     same_products = get_same_products(hot_product)
 
-    context = {
+    content = {
         'title': title,
-        'categories': categories,
-        'category': category,
-        'links_menu': get_links_menu(),
-        'products': products_paginator,
-        # 'basket': basket,
+        'links_menu': links_menu,
         'hot_product': hot_product,
         'same_products': same_products,
+        'basket': basket,
     }
-    return render(request, 'products_list.html', context=context)
 
+    return render(request, 'mainapp/products.html', content)
 
 @login_required
 def product(request, pk):
